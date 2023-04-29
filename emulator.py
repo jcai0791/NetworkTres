2aab326fb5b27ee1441268ca5f166515b9faa16b
Aiden Cohen <aidenc@rockhopper-03.cs.wisc.edu>
2023-04-29 11:31:06 -0500

    hello

--- emulator.py
+++ emulator.py
@@ -5,36 +5,26 @@ import csv
 from collections import defaultdict
 
 MAX_BYTES = 6000
-
+TOPOLOGY = defaultdict(lambda : [])
+ROUTING = defaultdict(lambda : (b'',0))
 ####### OLD STUFF #########
-
+def getType(packet):
+    pass
 #wraps inner packet (payload) with outer header
-def encapsulate(priority, src_ip, src_port, dest_ip, dest_port,payload):
-    packet = struct.pack(f"!B4sH4sHI{len(payload)}s",priority,src_ip,src_port,dest_ip,dest_port,len(payload),payload)
+def encapsulateLinkState(src_ip, src_port,seq_no,ttl,payload):
+    packet = struct.pack("!c4sHIII" +("8s"*len(payload)),b'L', src_ip,src_port,seq_no,len(payload), ttl , *[i[0].to_bytes(4,'big') + i[1].to_bytes(4,'big') for i in payload])
     return packet
 
 #returns outer header and inner packet separately
-def decapsulate(packet):
-    header = struct.unpack_from("!B4sH4sHI",packet)
-    length = header[5]
-    payload = struct.unpack_from(f"!{length}s",packet,offset=17)[0]
+def decapsulateLinkState(packet):
+    header = struct.unpack_from("!c4sHIII",packet)
+    convert = lambda x: (x[:4], int.from_bytes(x[4:], "big"))
+    length = header[4]
+    payload = [convert(struct.unpack_from(f"!8s",packet,offset=19+(8*i))[0])  for i in range(length)]
+
     return header,payload
 
-#Parses topology table
-def parseTable(fileName, selfHostname, selfPort):
-    with open(fileName, "r") as f:
-        d = defaultdict(lambda : ("",0,0,0))
-        reader = csv.reader(f,delimiter=' ')
-        for row in reader:
-            if(row[0]==selfHostname and int(row[1])==selfPort):
-                destHost = socket.inet_aton(socket.gethostbyname(row[2]))
-                destPort = int(row[3])
-                nextHost = socket.gethostbyname(row[4])
-                nextPort = int(row[5])
-                delay = int(row[6])/1000.0
-                lossProb = int(row[7])
-                d[(destHost, destPort)] = (nextHost,nextPort, delay, lossProb) 
-        return d
+
 
 #Sends packet to (ip,port)
 def sendPacket(packet, ip, port):
@@ -49,17 +39,47 @@ def sendLinkState():
     pass
 
 ####### REQUIRED FUNCTIONS ########
-def readtopology(filename):
-    pass
+def readtopology(filename,sip,sport):
+    with open(filename, "r") as f:
+        d = []
+        for row in csv.reader(f, delimiter=' '):
+            ip,port = row[0].split(",")
+            TOPOLOGY[(socket.inet_aton(ip),int(port))] = list(map(lambda x: (socket.inet_aton(x.split(",")[0]), int(x.split(",")[1]) ),  row[1:]))
+    buildForwardTable(socket.inet_aton(sip),int(sport))
 
 def createroutes():
     pass
 
-def forwardpacket():
-    pass
+def forwardpacket(packet,sip,sport):
+    t = struct.unpack_from("!c", packet, offset=0)
+    if(t == b'H'):
+        pass
+    elif(t == b'L'):
+        header,payload = decapsulateLinkState(packet)
+        if(header[4]==0):
+            return
+        packet = encapsulateLinkState(header[1],header[2], header[3], header[4]-1, payload)
+        for n in TOPOLOGY[(sip,sport)]:
+            sendPacket(packet,n[0],n[1])
 
-def buildForwardTable():
-    pass
+    else:
+        pass
+    header,_ = decapsulateLinkState(packet)
+
+def buildForwardTable(sip, sport):
+    global ROUTING
+    ROUTING = { i : i for i in TOPOLOGY[(sip,sport)] }
+    q = TOPOLOGY[(sip,sport)]
+    visited = {i for i in TOPOLOGY[(sip,sport)]}
+    visited.add((sip,sport))
+    while len(q) > 0:
+        node = q.pop(0)
+        for n in TOPOLOGY[node]:
+            if(not n in visited):
+                q.append(n)
+                visited.add(n)
+                ROUTING[n] = ROUTING[node]
+        
 
 
 
@@ -73,7 +93,9 @@ if __name__ == "__main__":
     port = int(args.port)
     # sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
     # sock.bind((socket.gethostname(), port))
-    readtopology(filename)
+    readtopology(filename,"1.0.0.0",1)
+    print(ROUTING)
+    print(TOPOLOGY)
 
 
 
